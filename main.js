19a8d3235031e6b96ef1dbc098c8ad5888954495
shketik-r <ruslans3101@gmail.com>
2024-02-21 13:11:06 +0300

    v2

--- main.js
+++ main.js
@@ -1,59 +1,86 @@
 class Modal {
-  constructor(wrap, btn) {
-    this.wrap = typeof wrap === "string" ? document.querySelector(wrap) : wrap;
-    this.btn = typeof btn === "string" ? document.querySelector(btn) : btn;
-    this.menu = this.wrap.firstElementChild;
-    this.#click();
-    this.#closeWindow();
+
+  constructor() {
+    this.btnAll = document.querySelectorAll("[data-open-modal]");
+    this.modal = document.querySelectorAll("[data-modal]");
+    this.open();
+    this.close();
   }
 
-  #click() {
-    this.btn.addEventListener('click', () => {
-      this.wrap.classList.toggle('active');
-      this.btn.classList.toggle('active');
-      this.wrap.classList.contains('active') ? this.#disableScroll() : this.#enableScroll();
-    })
+  open() {
+
+    if (this.btnAll.length > 0) {
+      this.btnAll.forEach(b => {
+        b.addEventListener('click', () => {
+          if (!b.classList.contains('active-btn-modal')) {
+            document.querySelector('.visibility-modal') ? document.querySelector('.visibility-modal').classList.remove('visibility-modal') : '';
+            document.querySelector('.active-btn-modal') ? document.querySelector('.active-btn-modal').classList.remove('active-btn-modal') : '';
+          }
+          b.classList.toggle('active-btn-modal');
+          this.requiredModal = document.querySelector(`[data-modal="${b.dataset.openModal}"]`);
+          if (this.requiredModal) {
+            this.requiredModal.classList.toggle('visibility-modal');
+            this.#disableScroll();
+          }
+        })
+      })
+    }
   }
 
-  #closeWindow() {
-    this.wrap.addEventListener('click', (event) => this.#hide(event));
+  close() {
+    if (this.modal.length > 0) {
+      this.modal.forEach(m => {
+        m.addEventListener('click', (event) => this.#hide(event));
+      })
+    }
   }
 
 
   #hide(event) {
-    if (this.menu.contains(event.target))
-      return;
-    this.wrap.classList.remove('active');
-    this.btn.classList.remove('active');
+    this.btnClose = event.currentTarget.querySelector("[data-close-modal]");
+    this.content = event.currentTarget.firstElementChild;
+
+    if (this.btnClose) {
+      if (this.content.contains(event.target) && !this.btnClose.contains(event.target)) {
+        return;
+      }
+      this.#removeVisibility(event.target);
+    } else {
+      if (this.content.contains(event.target)) {
+        return;
+      }
+      this.#removeVisibility(event.target);
+
+
+    }
+  }
+
+  #removeVisibility(modal) {
+    document.querySelector('.active-btn-modal') ? document.querySelector('.active-btn-modal').classList.remove('active-btn-modal') : '';
+    document.querySelector('.visibility-modal') ? document.querySelector('.visibility-modal').classList.remove('visibility-modal') : '';
     this.#enableScroll();
-    this.wrap.removeEventListener("click", event => this.#hide(event));
+    modal.removeEventListener("click", event => this.#hide(event));
   }
 
   #disableScroll() {
     const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
-    document.documentElement.classList.add('overflow');
+    document.documentElement.classList.add('lock-scroll');
     window.onscroll = function () {
       window.scrollTo(scrollLeft, scrollTop);
     };
   }
 
   #enableScroll() {
-    document.documentElement.classList.remove('overflow');
+    document.documentElement.classList.remove('lock-scroll');
     window.onscroll = function () { };
   }
 }
 
 
 
-const btn = document.querySelector('.open-menu');
-const wrap = document.querySelector('.menu-wrapper');
+new Modal();
 
-
-if (btn && wrap) {
-  const menu = new Modal(wrap, btn)
-
-}
 
 
 
