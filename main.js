a6bd56490459ee9e1ada4db110087c1372471b18
shketik-r <ruslans3101@gmail.com>
2024-02-22 15:42:08 +0300

    v1.01

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 class Modal {
 
-  constructor() {
-    this.btnAll = document.querySelectorAll("[data-open-modal]");
-    this.modal = document.querySelectorAll("[data-modal]");
+  constructor(option = {}) {
+
+    this.option = option;
+
+    this.ACTIVE_BUTTON = this.option.activeSelectButton ? this.option.activeSelectButton : "active-btn-modal";
+    this.ACTIVE_MODAL = this.option.activeSelectModal ? this.option.activeSelectModal : "visibility-modal";
+    this.BUTTON_DATA_NAME = this.option.buttonDataName ? this.option.buttonDataName : "open-modal";
+    this.MODAL_DATA_NAME = this.option.modalDataName ? this.option.modalDataName : "modal";
+    this.CLOSE_DATA_NAME = this.option.closeDataName ? this.option.closeDataName : "close-modal";
+
+    this.btnAll = document.querySelectorAll(`[data-${this.BUTTON_DATA_NAME}]`);
+    this.modal = document.querySelectorAll(`[data-${this.MODAL_DATA_NAME}]`);
+
     this.open();
     this.close();
   }
@@ -10,16 +20,18 @@ class Modal {
   open() {
 
     if (this.btnAll.length > 0) {
+
+      let newDataName = this.#convertString();
+
       this.btnAll.forEach(b => {
         b.addEventListener('click', () => {
-          if (!b.classList.contains('active-btn-modal')) {
-            document.querySelector('.visibility-modal') ? document.querySelector('.visibility-modal').classList.remove('visibility-modal') : '';
-            document.querySelector('.active-btn-modal') ? document.querySelector('.active-btn-modal').classList.remove('active-btn-modal') : '';
+          if (!b.classList.contains(this.ACTIVE_BUTTON)) {
+            this.#removeVisibility();
           }
-          b.classList.toggle('active-btn-modal');
-          this.requiredModal = document.querySelector(`[data-modal="${b.dataset.openModal}"]`);
+          b.classList.toggle(this.ACTIVE_BUTTON);
+          this.requiredModal = document.querySelector(`[data-${this.MODAL_DATA_NAME}="${b.dataset[newDataName]}"]`);
           if (this.requiredModal) {
-            this.requiredModal.classList.toggle('visibility-modal');
+            this.requiredModal.classList.toggle(this.ACTIVE_MODAL);
             this.#disableScroll();
           }
         })
@@ -37,29 +49,29 @@ class Modal {
 
 
   #hide(event) {
-    this.btnClose = event.currentTarget.querySelector("[data-close-modal]");
+    this.btnClose = event.currentTarget.querySelector(`[${this.CLOSE_DATA_NAME}]`);
     this.content = event.currentTarget.firstElementChild;
 
     if (this.btnClose) {
       if (this.content.contains(event.target) && !this.btnClose.contains(event.target)) {
         return;
       }
-      this.#removeVisibility(event.target);
+      this.#removeVisibility();
+      event.target.removeEventListener("click", event => this.#hide(event));
     } else {
       if (this.content.contains(event.target)) {
         return;
       }
-      this.#removeVisibility(event.target);
-
+      this.#removeVisibility();
+      event.target.removeEventListener("click", event => this.#hide(event));
 
     }
   }
 
-  #removeVisibility(modal) {
-    document.querySelector('.active-btn-modal') ? document.querySelector('.active-btn-modal').classList.remove('active-btn-modal') : '';
-    document.querySelector('.visibility-modal') ? document.querySelector('.visibility-modal').classList.remove('visibility-modal') : '';
+  #removeVisibility() {
+    document.querySelector('.' + this.ACTIVE_BUTTON) ? document.querySelector('.' + this.ACTIVE_BUTTON).classList.remove(this.ACTIVE_BUTTON) : '';
+    document.querySelector('.' + this.ACTIVE_MODAL) ? document.querySelector('.' + this.ACTIVE_MODAL).classList.remove(this.ACTIVE_MODAL) : '';
     this.#enableScroll();
-    modal.removeEventListener("click", event => this.#hide(event));
   }
 
   #disableScroll() {
@@ -74,6 +86,16 @@ class Modal {
   #enableScroll() {
     document.documentElement.classList.remove('lock-scroll');
     window.onscroll = function () { };
+  }
+
+  #convertString() {
+    let str = this.BUTTON_DATA_NAME;
+    let i = -1;
+    while ((i = str.indexOf('-', i + 1)) != -1) {
+      str = str.substr(0, i + 1) + str[i + 1].toUpperCase() + str.substr(i + 2);
+    }
+    str = str.replace(/[\.\-/\\\s]/g, '');
+    return str
   }
 }
 
